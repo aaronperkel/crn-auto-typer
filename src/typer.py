fbb20d57891109bf1f364db8e2e359d35d8c5d7e
aaronperkel <aaron.perkel27@gmail.com>
2025-04-08 22:14:10 -0400

    removed reader from typer

--- src/typer.py
+++ src/typer.py
@@ -4,24 +4,24 @@ typer.py script
 """
 
 import time
+import reader
 import pyautogui
 
 # List of CRNs to enter
-CRNS = [('12007', 'CS3050 - Software Engineering'),
-        ('12435', 'CS3930 - Computing Career Preparation'),
-        ('15862', 'MATH3766 - Chaos,Fractals&Dynmcal Syst'),
-        ('12568', 'NFS1053L - Basic Concepts of Foods LAB'),
-        ('12777', 'NFS1053 - Basic Concepts of Foods')]
+CRNS = reader.read_crns('./crns.txt')
 
-def main():
+def typer():
     # Type each CRN and press 'Tab' to move to the next text box
     for crn in CRNS:
-        pyautogui.typewrite(crn[0])
+        pyautogui.typewrite(crn)
         pyautogui.press('tab')  # Move to the next text box
         time.sleep(0.05)  # Small delay for stability
 
     # Submit the form by pressing 'Enter'
     pyautogui.press('enter')
-        
+
+def main():
+    print(CRNS)
+     
 if __name__ == '__main__':
     main()
